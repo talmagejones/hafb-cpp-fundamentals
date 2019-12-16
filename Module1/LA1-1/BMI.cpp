ee4ce8b01e31f2d33d8fca9918ea917d99d80c59
CCEClass1 <CCEClass1@ad.weber.edu>
2019-12-16 11:57:40 -0700

    Made an age rights script

--- Module1/LA1-1/BMI.cpp
+++ Module1/LA1-1/BMI.cpp
@@ -4,19 +4,23 @@ using namespace std;
 // Constants
 const float kMetersToInches = 39.37;
 const float kKilosToPunds = 2.204;
-const float kBMIImperial = 703;
+const int kBMIImperial = 703;
+const float kLowBMILimit = 18.5;
+const float kHighBMILimit = 24.9;
 
 int main()
 {
     // Calculate BMI: weight(kg)/[height(m)]^2
     // Floating point variables: float, double
     float weight = 0, height = 0, bmi = 0;  // open the space and intialize it 
+    /*
     cout << "Enter your weight in kg: " << endl;
     cin >> weight;
     cout << "Enter your height in m: " << endl;
     cin >> height;
     bmi = weight / (height * height);
-    cout << "Your BMI is: \n" << bmi << endl;
+    cout << "Your BMI (metric) is: \n" << bmi << endl;
+    */
 
     // Task: Now, convert it to imperial units
     // Calculate BMI (imperial): (weight(lbs)) * 703/[height(in)]^2
@@ -28,6 +32,19 @@ int main()
     cin >> height;
     bmi = weight * kBMIImperial / (height * height);
     cout << "Your BMI (imperial) is: \n" << bmi << endl;
+
+    // Task: Print if they are in the "good" range: BMI is in the 18.5 to 24.9
+    // && (AND) to test two conditions
+    if(bmi > kLowBMILimit && bmi < kHighBMILimit)
+    {
+        cout << "\nYour BMI of " << bmi << " is good\n" << endl;
+    }
+    else
+    {
+        cout << "\nYour BMI of " << bmi << " is bad\n" << endl;
+    }
+    
+    cout << "Thank you for using BMI program" << endl;
 
     return 0;
 }
