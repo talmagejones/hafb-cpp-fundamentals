4802c42a4c282c675ffa74faedacd1a040d81b0b
CCEClass1 <CCEClass1@ad.weber.edu>
2019-12-16 11:16:40 -0700

    Added imperial units to BMI Also added constants

--- Module1/LA1-1/BMI.cpp
+++ Module1/LA1-1/BMI.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 using namespace std;
 
+// Constants
+const float kMetersToInches = 39.37;
+const float kKilosToPunds = 2.204;
+const float kBMIImperial = 703;
+
 int main()
 {
     // Calculate BMI: weight(kg)/[height(m)]^2
@@ -12,6 +17,17 @@ int main()
     cin >> height;
     bmi = weight / (height * height);
     cout << "Your BMI is: \n" << bmi << endl;
+
+    // Task: Now, convert it to imperial units
+    // Calculate BMI (imperial): (weight(lbs)) * 703/[height(in)]^2
+    // 1m = 39.37in
+    // 1kg = 2.204lbs
+    cout << "Enter your weight in lbs: " << endl;
+    cin >> weight;
+    cout << "Enter your height in inches: " << endl;
+    cin >> height;
+    bmi = weight * kBMIImperial / (height * height);
+    cout << "Your BMI (imperial) is: \n" << bmi << endl;
 
     return 0;
 }
