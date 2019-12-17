cb98ca8245668aae1f3496d142aee96ff5b0a25e
Talmage Jones <talmagejones@ad.weber.edu>
2019-12-17 15:08:26 -0700

    did a bunch of stuff with strings

--- Module2/LA2-1/src/main.cpp
+++ Module2/LA2-1/src/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
-
-using namespace std;
+#include <string>
 
 /**
  * @brief Shows how to define and use a string object
@@ -12,24 +11,61 @@ using namespace std;
  */
 int main() {
   // create a variable of type string
+  std::string name = "BYU Cosmo";
 
   // print it out
+  std::cout << name << std::endl;
 
-  // get a string from a user
+  // // get a string from a user
+  // std::cout << "Enter your full name: " << std::endl;
+
+  // // to get strings with spaces, use getline() instead
+  // getline(std::cin, name);
+  // std::cout << "Hi " << name << std::endl;
 
   // add two strings together
+  std::string title = "Dr.";
+  std::string formal;
+  formal = title + " " + name;
+  std::cout << "Hi: " << formal << std::endl;
 
   // how long is the string
+  std::cout << "The length of name is " << name.size() << std::endl;
+  std::cout << "The length of title is " << title.size() << std::endl;
+  std::cout << "The length of formal is " << formal.size() << std::endl;
+  std::cout << "The length of formal is " << formal.length() << std::endl; // size is the same as length
+
 
   // use [] to get an individual character
+  std::cout << "The first character of name is " << name[0] << std::endl;
+  std::cout << "The third character of name is " << name[2] << std::endl;
 
   // get name and commute information from a user
-
-  // get commute information and name from a user
+  int minutes, miles;
+  std::cout << "Enter your full name: " << std::endl;
+  getline(std::cin, name);
+  std::cout << "How long is your commute in minutes? ";
+  std::cin >> minutes;
+  std::cout << "How long is your commute in miles? ";
+  std::cin >> miles;
+  std::cout << name << " has a " << minutes << " minute, " << miles 
+    << " mile commute." << std::endl;
 
   // the string function find
+  std::string story = 
+    "adsfds dhdf gf dsfg sfdg qewro werf wfe sjdf sdf sdfewioqew nqew dewn ewewnew ew ewd"
+    "zsdgff  sdfgdf df gd dfgdfs dsfg dfg.dfg dgh.d ger gyre .ergr fdgdfg";
+
+  std::cout << "wfe is at location: " << story.find("wfe") << std::endl;
 
   // the string function replace
+  std::cout << story << std::endl;
+  std::string nick = "wfe";
+  std::string new_nick = "XXXXX";
+  story.replace(story.find(nick), nick.size(), new_nick); // replace wfe with xxxxx
+  std::cout << story << std::endl;
+
+
 
   return 0;
 }
