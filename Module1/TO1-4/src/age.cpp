9d497b073bc4409a6791ddec60d79df7340983b3
Talmage Jones <talmagejones@ad.weber.edu>
2019-12-17 11:44:34 -0700

    made pass by value functions

--- Module1/TO1-4/src/age.cpp
+++ Module1/TO1-4/src/age.cpp
@@ -1,1 +1,11 @@
 #include "age.h"
+
+void ByValueAgeIn5Years(int age) // pass by value
+{
+    age += 5;
+}
+
+int CurrentAge(int age) // pass by value
+{
+    return age;
+}
--- Module1/TO1-4/src/age.h
+++ Module1/TO1-4/src/age.h
@@ -1,5 +1,7 @@
 #ifndef AGE_H_
 #define AGE_H_
 
+void ByValueAgeIn5Years(int age);
+int CurrentAge(int age);
 
 #endif /* !AGE_H_ */
