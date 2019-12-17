5c4f3cdd1b02fb7fdaa78ec77efbd210cd1c5d94
Talmage Jones <talmagejones@ad.weber.edu>
2019-12-17 14:10:26 -0700

    added car hp functions

--- Module1/TO1-5/src/cars.cpp
+++ Module1/TO1-5/src/cars.cpp
@@ -1,4 +1,22 @@
 #include <iostream>
 #include "cars.h"
+
+int HorsePower()       // "standard" namespace
+{
+    return 150;
+}
 // using namespace lamborghini;
+int lamborghini::HorsePower() 
+{
+    return 760;
+}
+
+void lamborghini::cout()
+{
+    std::cout << "My car is a Lamborghini" << std::endl;
+}
 // using namespace porsche;
+int porsche::HorsePower() 
+{
+    return 700;
+}
--- Module1/TO1-5/src/main.cpp
+++ Module1/TO1-5/src/main.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-
-#include <iostream>
 #include "cars.h"
 // using namespace std;
 // using namespace lamborghini;
@@ -11,6 +9,11 @@ int main()
 {
     std::cout << "The lamborghini's horse power: " << lamborghini::HorsePower() << std::endl;
     std::cout << "The porsche's horse power: " << porsche::HorsePower() << std::endl;
-    std::cout << "A regular car's horse power: " << HorsePower() << std::endl;
+    // this is the nameless namespace
+    std::cout << "A regular car's horse power: " << ::HorsePower() << std::endl;
+    //std::cout << "A regular car's horse power: " << HorsePower() << std::endl;
+
+    lamborghini::cout();
+    
     return 0;
 }
